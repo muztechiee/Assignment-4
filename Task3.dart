1f56978310911971eb4b3174c35abdc9ed1d4e76
muztechiee <muztechiee@gmail.com>
2025-11-09 07:30:03 +0500

    Task3

---
+++ Task3.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> originalList = ["apple", "banana", "orange", "grape"];
+
+  List<String> reversedList = List.of(originalList.reversed);
+
+  print(originalList);
+  print(reversedList);
+}
